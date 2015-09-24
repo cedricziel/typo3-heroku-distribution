55aa48b10c749d78fe48c77257d5a0e3e90a7666
Cedric Ziel <cedric@cedric-ziel.com>
2015-09-25 00:57:51 +0200

    [TASK] Add conf templates

---
+++ web/typo3conf/AdditionalConfiguration.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Loads the environment from the .env file in the root
+ * directory
+ */
+$dotenv = new Dotenv\Dotenv(__DIR__ . '/../');
+$dotenv->load();
+
+/**
+ * You have configuration from the environment available here
+ * via getenv
+ */
+
+/**
+ * NO EDITS BEYOND THIS POINT
+ *
+ * AutoConfiguration happens beyond this point
+ */
+if (file_exists(dirname(__FILE__) . '/AutoConfiguration.php')) {
+    include_once dirname(__FILE__) . '/AutoConfiguration.php';
+}
